cdc92cc80d6f5a245dfd234b323f726f09d7c221
rafarobert <rafarobertgu@gmail.com>
2018-02-06 02:56:09 -0400

    migrations sys

--- modules/base/dashboard/Ctrl_Dashboard.php
+++ modules/base/dashboard/Ctrl_Dashboard.php
@@ -6,28 +6,30 @@
  * Time: 12:34
  */
 
-class Ctrl_Dashboard extends Estic_Controller {
+class Ctrl_Dashboard extends Base_Controller {
 
     function __construct(){
         parent::__construct();
+        $this->load->model('model_sessions');
+        $this->load->model('model_usuarios');
+        $this->load->library('session');
     }
 
     public function index(){
-        if($this->validate_modulo('estic','users')){
+        if($this->validate_modulo('base','usuarios')){
             $id_user = $this->session->getId();
-            $oUser = $this->model_users->get($id_user);
+            $oUser = $this->model_usuarios->get($id_user);
             if (is_object($oUser)){
                 $this->data['oUser'] = $oUser;
                 $this->data['subview'] = 'estic/dashboard/index';
+            } else {
+                $this->data['subLayout'] = 'start';
             }
-            $this->load->view("_layout_estic", $this->data);
         } else {
-            $this->data['subview_layout'] = 'estic/_construccion';
-            $this->load->view("_layout_construccion", $this->data);
+            $this->data['subLayout'] = 'admin/_construccion';
         }
     }
     public function modal(){
         $this->data['layout'] = 'modal_estic';
-        $this->load->view("_layout_estic", $this->data);
     }
 }
