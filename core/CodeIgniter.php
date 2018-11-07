9b30d813ecfd334e52affa9a00290677c0382702
Rafael Gutierrez <rafarobertgu@gmail.com>
2018-11-07 17:44:13 -0400

    Revert "Configuraciones para la puesta a produccion"
    This reverts commit e17c3ce5b08e6a139f4f28c1dcecbe06be681a81.

--- core/CodeIgniter.php
+++ core/CodeIgniter.php
@@ -229,17 +229,14 @@ switch (ENVIRONMENT)
         break;
 
     case 'production':
-//        if (version_compare(PHP_VERSION, '5.3', '>='))
-//        {
-//            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-//        }
-//        else
-//        {
-//            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-//        }
-//        break;
-        error_reporting(-1);
-        ini_set('display_errors', 1);
+        if (version_compare(PHP_VERSION, '5.3', '>='))
+        {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+        }
+        else
+        {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+        }
         break;
 
     default:
