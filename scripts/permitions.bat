fa3f1dec414cc3344bc8abbb8fed5e3f55a95395
rafael Gutierrez Gaspar <rafarobertgu@gmail.com>
2019-05-28 18:28:52 -0400

    no message

--- scripts/permitions.bat
+++ scripts/permitions.bat
@@ -3,7 +3,13 @@ icacls ../../../ /grant rafae:(d,wdac)
 cd ../..
 
 TAKEOWN /F app /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
-TAKEOWN /F orm /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
+TAKEOWN /F orm/classes /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
+TAKEOWN /F orm/config /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
+TAKEOWN /F orm/crud /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
+TAKEOWN /F orm/map /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
+TAKEOWN /F orm/migrations /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
+TAKEOWN /F orm/schema /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
+TAKEOWN /F orm/sql /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
 TAKEOWN /F assets /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
 TAKEOWN /F icon /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
 TAKEOWN /F plugins /R /S "ibost-lt11osi" /U "rafae" /P "inspiron15R-"
